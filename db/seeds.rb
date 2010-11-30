a79e9b8281d78c0a55a3611b575882f6bace1041
Maximiliano Dello Russo <mdellorusso@aonken.com.ar>
2010-11-30 10:19:38 -0300

    Se agregan los parametros comunes al modelo de Sector, Empresa y Rubros (ver db/seeds.rb)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+
+# En principio las empresas serian  Laindell SRL  y  Metalurgica La Toma SA
+# Los sectores : Tejeduria /  Mantenimiento / Recubrimiento / Embalaje / Fabrica de piletas / Caños / 
+# 							 Expedicion / Administracion / Compras / Ventas.
+# Los rubros: Materias Primas / Insumos / Consumibles / Varios / Servicios / Maquinas y herramientas
+
+# FIXME Analizar y arreglar la dificultad con el encoding y los caracteres de acentuación
+empresas = Empresa.create([{:nombre => 'Laindell SRL'}, {:nombre => 'Metalurgica La Toma SA'}])
+
+sectores = Sector.create([{:nombre => 'Tejeduria'}, {:nombre => 'Mantenimiento'}, 
+							{:nombre => 'Recubrimiento'}, {:nombre => 'Embalaje'}, 
+							{:nombre => 'Fabrica de piletas'}, {:nombre => 'Canos'},
+						  {:nombre => 'Expedicion'}, {:nombre => 'Administracion'},
+						  {:nombre => 'Compras'}, {:nombre => 'Ventas'}])
+						  
+rubros = Rubro.create([{:nombre => 'Materias Primas'}, {:nombre => 'Insumos'}, 
+							{:nombre => 'Consumibles'}, {:nombre => 'Varios'}, {:nombre => 'Servicios'},
+							{:nombre => 'Maquinas y herramientas'}])
