b2bc9c68559d59ae337d27611be9c3a33ce0c61e
Maximiliano Dello Russo <mdellorusso@aonken.com.ar>
2011-01-27 23:15:35 -0300

    Se realizaron cambios en el seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,20 +40,20 @@ end
 # SECTORES -------------------------------------------------------------------------------------------
 # ES MUY IMPORTANTE QUE "Expedición", "Administración" y "Compras" reciban como ID el 7, 8 y 9 respectivamente.
 # Ver modelo sector
-sectores = ["Tejeduría", "Mantenimiento", "Recubrimiento",
-	"Embalaje", "Fábrica de piletas", "Caños", "Expedición",
-	"Administración", "Compras", "Ventas" ]
+#sectores = ["Tejeduría", "Mantenimiento", "Recubrimiento",
+#	"Embalaje", "Fábrica de piletas", "Caños", "Expedición",
+#	"Administración", "Compras", "Ventas" ]
 
-sectores.each do |sector|
-	unless Sector.find_by_nombre(sector)
-		sector = Sector.new({:nombre => sector})
-		sector.responsables << responsable
-		unless sector.valid?
-			puts "Inválido: #{sector.errors}"
-		end
-		sector.save
-	end
-end
+#sectores.each do |sector|
+#	unless Sector.find_by_nombre(sector)
+#		sector = Sector.new({:nombre => sector})
+#		sector.responsables << responsable
+#		unless sector.valid?
+#			puts "Inválido: #{sector.errors}"
+#		end
+#		sector.save
+#	end
+#end
 
 #sectores.each do |nombre_sector|
 #	sector = Sector.where(:nombre => nombre_sector).first
