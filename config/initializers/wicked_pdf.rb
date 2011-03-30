60df7a9dab031389635ff18c9cbce177f63e9651
Maximiliano Dello Russo <mdellorusso@aonken.com.ar>
2011-03-30 11:50:49 -0300

    Se realiza un cambio para la generación de PDF en heroku

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -2,5 +2,5 @@ WICKED_PDF = {
   #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
   #:layout => "pdf.html",
   :exe_path => Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
-} if RAILS_ENV == 'production'
+} 
 
