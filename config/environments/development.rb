a8789469ed96767d64ae6d526bf0c67973f4f3f2
Maximiliano Dello Russo <mdellorusso@aonken.com.ar>
2011-05-19 12:29:23 -0300

    Se elimina el envío de mails en el entorno de desarrollo

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,9 @@ Rms::Application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
-  config.action_mailer.delivery_method = :smtp
+#  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :test
+  
 	config.action_mailer.smtp_settings = {
 		:address              => "smtp.gmail.com",
 		:port                 => 587,
