7d8691a958076163a853e14c39a6c2a7b6255ba3
Maximiliano Dello Russo <mdellorusso@aonken.com.ar>
2011-06-03 15:48:22 -0300

    Se agrega una marca sobre los requerimientos que poseen iva y los que no. Petición #95

--- app/helpers/requerimientos_helper.rb
+++ app/helpers/requerimientos_helper.rb
@@ -1,2 +1,6 @@
 module RequerimientosHelper
+  def iva_mark(requerimiento)
+    aprobado = requerimiento.presupuestos.exists? ? requerimiento.presupuestos.aprobado : nil
+    aprobado.con_iva? ? ' (*)' : ' ()' if aprobado      
+  end  
 end
