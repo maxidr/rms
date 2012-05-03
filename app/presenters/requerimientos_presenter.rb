8b2559f6a2796e87a5a601c93db3a1160ec85b65
Maximiliano Dello Russo <mdellorusso@aonken.com.ar>
2012-05-03 11:40:12 -0300

    Se agrega la información de los presupuestos aprobados en el excel de requerimientos

--- app/presenters/requerimientos_presenter.rb
+++ app/presenters/requerimientos_presenter.rb
@@ -2,6 +2,7 @@
 class RequerimientosPresenter
 
   include ActionView::Helpers::TranslationHelper
+  include ActionView::Helpers::NumberHelper
 
   def initialize(requerimientos)
     @reqs = requerimientos
@@ -10,17 +11,28 @@ class RequerimientosPresenter
   def to_xls
     book = Spreadsheet::Workbook.new
     sheet = book.create_worksheet :name => 'Requerimientos'
-    add_title(sheet)
+    titles = ["N°", "Fecha", "Solicitante", "Empresa", "Sector", "Rubro", "Estado", 
+      "Proveedor del presupuesto", "Moneda", "Monto total final", "Condición de pago", "Destalles del presupuesto"]
+    add_titles(sheet, titles)
     load_content(sheet, @reqs) do |rqm|
-      [
+      row = [
         rqm.id, 
         l(rqm.created_at, :format => :short), 
         rqm.solicitante.try(:to_s),
         rqm.empresa.try(:to_s),
         rqm.sector.try(:to_s),
         rqm.rubro.try(:to_s),
-        rqm.estado.try(:to_s) 
-      ]
+        rqm.estado.try(:to_s) ]
+      if presupuesto = rqm.presupuestos.aprobado
+        row.concat [
+          presupuesto.proveedor.razon_social,
+          presupuesto.moneda.simbolo, 
+          number_with_precision(presupuesto.monto_total),
+          presupuesto.condicion_pago.nombre,
+          presupuesto.detalle
+        ]
+      end
+      row
     end
     stringify(book)
   end
@@ -41,10 +53,10 @@ class RequerimientosPresenter
     end
   end
 
-  def add_title(sheet)
+  def add_titles(sheet, titles)
     title_format = Spreadsheet::Format.new :weight => :bold, :align => :center
     sheet.row(0).default_format = title_format
-    sheet.row(0).concat %w( N° Fecha Solicitante Empresa Sector Rubro Estado )
+    sheet.row(0).concat titles
     sheet
   end
 end
