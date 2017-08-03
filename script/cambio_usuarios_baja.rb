54d4128dbed97fce0dfbc6e5e12078e0bd5746c3
luis petek <lmpetek@gmail.com>
2017-08-03 08:37:21 -0300

    marcado de requerimientos como terminados (10)

--- script/cambio_usuarios_baja.rb
+++ script/cambio_usuarios_baja.rb
@@ -3,9 +3,12 @@ Requerimiento.all.each do |item|
   if item.solicitante.blank?
     indice += 1
     puts "#{item.id} #{item.solicitante_id} #{indice}"
-    #if item.solicitante_id == 6
-      item.solicitante_id = 39
-      item.save
-    #end
+    item.solicitante_id = 39
+    item.save
   end
+end
+
+Requerimiento.where(:solicitante_id => 39).where('estado < 5').each do |item|
+  item.estado = 10
+  item.save
 end
